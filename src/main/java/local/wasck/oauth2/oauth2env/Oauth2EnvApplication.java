3a423769e4fbd206f139b5228e443db310259afd
Waldemar Schik <Waldemar.Schik@gmail.com>
2020-10-16 19:46:51 +0200

    Initialize spring project

---
+++ src/main/java/local/wasck/oauth2/oauth2env/Oauth2EnvApplication.java
@@ -0,0 +1,13 @@
+package local.wasck.oauth2.oauth2env;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2EnvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2EnvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/local/wasck/oauth2/oauth2env/Oauth2EnvApplicationTests.java
@@ -0,0 +1,13 @@
+package local.wasck.oauth2.oauth2env;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2EnvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
